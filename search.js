5c1bb67c34a2fabdda8d0ab2d45417647ec871b0
Lena Ryan <alissadyiphil@gmail.com>
2019-05-30 14:30:49 +0300

    Несколько тестов + проверка на ошибки в функции

--- search.js
+++ search.js
@@ -1,13 +1,24 @@
 module.exports = (arr, num) => {
+  /* На первой проверке можно было выбросить ошибку,
+  *  подобную той, которая выходит на второй проверке
+  */
+  if (!Array.isArray(arr) || !Number.parseInt(num)) return -1;
+  
+  if (!arr.every(el => Number.parseInt(el))) {
+    throw new TypeError('array should include numbers')
+  }
+
   let leftIndex = arr[0];
   let rightIndex = arr.length - 1;
   let midIndex = 0;
+
   if (arr[leftIndex] == num) {
     return leftIndex;
   }
   if (arr[rightIndex] == num) {
     return rightIndex;
   }
+
   while (leftIndex < rightIndex - 1) {
     midIndex = Math.floor((rightIndex + leftIndex) / 2);
     if (arr[midIndex] == num) {
@@ -20,5 +31,6 @@ module.exports = (arr, num) => {
       leftIndex = midIndex;
     }
   }
+  
   return -1;
 }
--- test/searchTest.js
+++ test/searchTest.js
@@ -1,6 +1,38 @@
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 var search = require('../search');
 
 describe('Binary search function testing', function() {
+    let arr = [1, 3, 5, 7, 8, 9, 15, 30, 34, 55];
+    let num = 30;
 
+    it('the first argument is an array', function() {
+        assert.isArray(arr);
+    })
+
+    it('the second argument is a number', function () {
+        assert.isNumber(num);
+    })
+
+    it('returns an element index', function () {
+        expect(search(arr, num)).to.equal(7);
+    })
+
+    it('returns -1 if the first argument is not an array', function() {
+        expect(search(10, num)).to.equal(-1);
+    })
+
+    it('returns -1 if the second argument is not a number', function () {
+        expect(search(arr, 'a')).to.equal(-1);
+    })
+
+    it('returns -1 if the number is not in the array', function () {
+        expect(search(arr, 4)).to.equal(-1);
+    })
+
+    it('throws an error if not every element of the array is a number', function () {
+        expect(function() {
+            search([1, 2, 'a'], num)
+        }).to.throw(TypeError, 'array should include numbers');
+    })
 })
